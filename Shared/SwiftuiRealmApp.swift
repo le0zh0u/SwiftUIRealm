fc48cf966362101e2e35d1ab92d7b940f64fdeda
leozhou <492753662@qq.com>
2022-05-23 16:47:11 +0800

    Initial Commit

---
+++ Shared/SwiftuiRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiRealmApp.swift
+//  Shared
+//
+//  Created by 周椿杰 on 2022/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
